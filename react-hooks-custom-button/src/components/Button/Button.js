61c5f99ba6b9a65097a81384ae0e4497807d7503
Caroline Courtney <caroline@carolinecourtney.com>
2019-12-07 15:10:05 -0500

    styleguidist portion might be out of date, but it looks nonetheless like good docs for react components

--- react-hooks-custom-button/src/components/Button/Button.js
+++ react-hooks-custom-button/src/components/Button/Button.js
@@ -1,20 +1,20 @@
-import React from 'react';
-import './../../styles/common.scss';
 
+import React from "react";
+import "./../../styles/common.scss";
+import "./Button.scss";
 
-function Button({children, type}) {
-
+function Button({ children, type, onClick }) {
   let localClass = "primary";
-  if (type === "primary") {
-    localClass = "primary"
-  }
   if (type === "disabled") {
     localClass = "disabled";
   }
-  const cls = `bgYellow fontBlack pad8 width100 radius15 ${localClass}`.trim();
+
+  const cls = `radius15 bgYellow fontBlack pad8 width100 ${localClass}`.trim();
   return (
     <div className="btnClass">
-      <button className={cls}>{children}</button>
+      <button onClick={onClick} className={cls}>
+        {children}
+      </button>
     </div>
   );
 }
